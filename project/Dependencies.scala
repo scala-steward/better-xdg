58d56574636a2bbc6b25c37de9786a6f06717502
Edward Samson <edward@samson.ph>
2020-05-15 22:29:59 +0800

    Upgrade to better-files 3.9.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
 
   object Libs {
 
-    val betterFiles = "com.github.pathikrit" %% "better-files" % "3.8.0"
+    val betterFiles = "com.github.pathikrit" %% "better-files" % "3.9.0"
 
     val config = "com.typesafe" % "config" % "1.4.0"
 
