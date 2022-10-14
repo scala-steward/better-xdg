f25f2c15867dcce802429f973560664b55cd7766
Scala Steward <scala_steward@virtuslab.com>
2022-10-14 20:08:40 +0000

    Update scalatest to 3.2.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
 
     val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
 
-    val scalaTest = "org.scalatest" %% "scalatest" % "3.2.13"
+    val scalaTest = "org.scalatest" %% "scalatest" % "3.2.14"
   }
 
   class DepsBuilder(configuration: Configuration, dependencies: Seq[ModuleID]) {
