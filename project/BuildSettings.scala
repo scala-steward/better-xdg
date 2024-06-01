1c1fdf95151d668171a920bb528f416fbce42e45
Edward Samson <edward@samson.ph>
2024-06-01 09:57:17 +0800

    Remove custom scalac options
    sbt-tpolecat already takes care of these.

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -35,22 +35,6 @@ object BuildSettings {
     config(Project(projectId, dir))
   }
 
-  final val DefaultScalacOptions = Seq(
-    "-encoding",
-    "UTF-8", // Specify character encoding used by source files.
-    "-Ybackend-parallelism",
-    s"${sys.runtime.availableProcessors()}", // maximum worker threads for backend
-    "-Ybackend-worker-queue",
-    s"${sys.runtime.availableProcessors() * 2}", // backend threads worker queue size
-    "-Woctal-literal", // Warn on obsolete octal syntax.
-    "-Xlint:nonlocal-return", // A return statement used an exception for flow control.
-    "-Xlint:implicit-not-found", // Check @implicitNotFound and @implicitAmbiguous messages.
-    "-Xlint:serial", // @SerialVersionUID on traits and non-serializable classes.
-    "-Xlint:valpattern", // Enable pattern checks in val definitions.
-    "-Xlint:eta-zero", // Warn on eta-expansion (rather than auto-application) of zero-ary method.
-    "-Xlint:eta-sam" // Warn on eta-expansion to meet a Java-defined functional interface that is not explicitly annotated with @FunctionalInterface.
-  )
-
   final val DefaultJavacOptions = Seq(
     "-deprecation",
     "-encoding",
@@ -61,19 +45,6 @@ object BuildSettings {
   )
 
   lazy val defaultSettings: Seq[Setting[_]] = Seq(
-    scalacOptions ++= DefaultScalacOptions,
-    scalacOptions ++= (
-      // Fail the compilation if there are any warnings.
-      if (sys.env.contains("STRICT")) {
-        Seq("-Xfatal-warnings")
-      } else {
-        Nil
-      }
-    ),
-    Compile / console / scalacOptions --= Seq(
-      "-Wunused:imports",
-      "-Xfatal-warnings"
-    ),
     javacOptions ++= DefaultJavacOptions,
     Test / logBuffered := false
   )
